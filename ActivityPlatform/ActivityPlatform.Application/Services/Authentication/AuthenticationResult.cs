ea43777ff72599d408400530085b5e5b141ac5d5
ederbf <ederbf@gmail.com>
2023-09-18 15:21:59 +0200

    remove unnecessary usings

--- ActivityPlatform/ActivityPlatform.Application/Services/Authentication/AuthenticationResult.cs
+++ ActivityPlatform/ActivityPlatform.Application/Services/Authentication/AuthenticationResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ActivityPlatform.Application.Services.Authentication
+﻿namespace ActivityPlatform.Application.Services.Authentication
 {
     public record AuthenticationResult(
         Guid Id,
