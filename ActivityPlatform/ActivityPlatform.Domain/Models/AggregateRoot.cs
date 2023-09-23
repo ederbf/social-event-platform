bc4b7999342a4bba65f7514055247423859c7268
ederbf <ederbf@gmail.com>
2023-09-23 11:37:36 +0200

    DDD base classes

---
+++ ActivityPlatform/ActivityPlatform.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace ActivityPlatform.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id) { }
+    }
+}
