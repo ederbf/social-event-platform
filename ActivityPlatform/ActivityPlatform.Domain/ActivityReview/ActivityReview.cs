bb4cc720b392d25b4d84ca819654b7264499e73c
ederbf <ederbf@gmail.com>
2023-09-24 21:59:32 +0200

    activityReview aggregate

---
+++ ActivityPlatform/ActivityPlatform.Domain/ActivityReview/ActivityReview.cs
@@ -0,0 +1,63 @@
+﻿using SocialEventPlatform.Domain.Activity.ValueObjects;
+using SocialEventPlatform.Domain.ActivityReview.ValueObjects;
+using SocialEventPlatform.Domain.Common.Models;
+using SocialEventPlatform.Domain.Common.ValueObjects;
+using SocialEventPlatform.Domain.Host.ValueObjects;
+using SocialEventPlatform.Domain.SocialEvent.ValueObjects;
+using System.Xml.Linq;
+
+namespace SocialEventPlatform.Domain.ActivityReview
+{
+    public sealed class ActivityReview : AggregateRoot<ActivityReviewId>
+    {
+        public Rating Rating { get; }
+        public string Comment { get; }
+        public HostId HostId { get; }
+        public ActivityId ActivityId { get; }
+        public GuestId GuestId { get; }
+        public SocialEventId SocialEventId { get; }
+        public DateTime CreatedDateTime { get; }
+        public DateTime UpdatedDateTime { get; }
+
+        private ActivityReview(
+            ActivityReviewId activityReviewId,
+            Rating rating,
+            string comment,
+            HostId hostId,
+            ActivityId activityId,
+            GuestId guestId,
+            SocialEventId socialEventId,
+            DateTime createdDateTime,
+            DateTime updatedDateTime) : base(activityReviewId)
+        {
+            Rating = rating;
+            Comment = comment;
+            HostId = hostId;
+            ActivityId = activityId;
+            GuestId = guestId;
+            SocialEventId = socialEventId;
+            CreatedDateTime = createdDateTime;
+            UpdatedDateTime = updatedDateTime;
+        }
+
+        public static ActivityReview Create(
+            Rating rating,
+            string comment,
+            HostId hostId,
+            ActivityId activityId,
+            GuestId guestId,
+            SocialEventId socialEventId)
+        {
+            return new(
+                ActivityReviewId.CreateUnique(),
+                rating,
+                comment,
+                hostId,
+                activityId,
+                guestId,
+                socialEventId,
+                DateTime.UtcNow,
+                DateTime.UtcNow);
+        }
+    }
+}
